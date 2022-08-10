4b1bbf769ed3641ea11700fd41535760e5a6a64d
atanteck <atanteck@caltech.edu>
2022-08-10 16:35:12 -0700

    Added application to render random RGB to screen

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "rayTracingDemo"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "rayTracingDemo"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "rayTracingDemo"
